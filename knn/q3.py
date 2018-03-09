01dc7970df0d6d9ca5ef706a5a426411ad56a049
Yatharth Garg <ygarg020@gmail.com>
2018-03-08 23:17:55 -0600

    Add files via upload

---
+++ knn/q3.py
@@ -0,0 +1,158 @@
+# Name - Yatharth Garg
+# Class - CSE 6363
+# ID - 1001510282
+
+import csv
+import random
+import math
+import operator
+
+#########################################################
+#loading the dataset
+#########################################################
+
+def load_data(filename, split, training=[], test=[]):
+    with open(filename, 'rt') as csvfile:
+        row = csv.reader(csvfile)
+        dataset = list(row)
+        for i in range(len(dataset) - 1):
+            for j in range(4):
+                dataset[i][j] = float(dataset[i][j])
+            if random.random() < split:
+                training.append(dataset[i])
+            else:
+                test.append(dataset[j])
+
+#########################################################
+#implementing euclidean distance
+#########################################################
+
+def euclidean_distance(neighbor, host, length):
+    distance = 0
+    for i in range(length):
+        distance += pow((neighbor[i] - host[i]), 2)
+    return math.sqrt(distance)
+
+#########################################################
+#knn classifier
+#########################################################
+
+def myknnclassify(trainingSet, testInstance, k):
+    distances = []
+    length = len(testInstance) - 1
+    for i in range(len(trainingSet)):
+        dist = euclidean_distance(testInstance, trainingSet[i], length)
+        distances.append((trainingSet[i], dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors = []
+    for i in range(k):
+        neighbors.append(distances[i][0])
+    return neighbors
+
+#########################################################
+#knn regressor
+#########################################################
+
+def myknnregress(train, test, k):
+    distances = []
+    length = len(test) - 1
+    for i in range(len(train)):
+        dist = euclidean_distance(test, train[i], length)
+        distances.append((train[i], dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors = []
+    for i in range(k):
+        neighbors.append(distances[i][0])
+    return neighbors
+
+
+######################################################################
+#selecting the maximum value for the neighbor in the classification
+######################################################################
+
+def get_classify(neighbors):
+    classVotes = {}
+    for i in range(len(neighbors)):
+        response = neighbors[i][-1]
+        if response in classVotes:
+            classVotes[response] += 1
+        else:
+            classVotes[response] = 1
+    sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedVotes[0][0]
+
+#########################################################
+#selecting the mean for the neighbor in the classification
+#########################################################
+
+def get_regress(neighbors,u):
+    class_list = []
+    for i in range(len(neighbors)):
+        response = neighbors[i][-1]
+        class_list.append(response)
+        class_list = list(map(int, class_list))
+    value = sum(class_list)
+    mean = float((value)/(u))
+    return str(math.floor(mean))
+
+
+#########################################################
+#calculating accuracy
+#########################################################
+
+def get_accuracy(testSet, predictions):
+    correct = 0
+    for i in range(len(testSet)):
+        if testSet[i][-1] == predictions[i]:
+            correct += 1
+    return (correct / float(len(testSet))) * 100.0
+
+
+
+def main():
+    # prepare dataset and splitting into 60% training dataset and 40% testing dataset
+    X = []
+    test = []
+    split = 0.80
+
+##################################################################################
+# comment out the bottom code for the implementation of knn classification
+##################################################################################
+
+
+    # load_data('iris.csv', split, X, test)
+    # print('Length of training dataset: ' + repr(len(X)))
+    # print('Length of testing dataset: ' + repr(len(test)))
+    # predictions = []
+    # k_classify = int(input("Enter the value of k (knn classify): "))
+    # for x in range(len(test)):
+    #     neighbors = myknnclassify(X, test[x], k_classify)
+    #     print(neighbors)
+    #     result = get_classify(neighbors)
+    #     predictions.append(result)
+    #     print('Predicted=' + repr(result) + ', Actual=' + repr(test[x][-1]))
+    # accuracy = get_accuracy(test, predictions)
+    # print('Accuracy: ' + repr(accuracy) + '%')
+
+#-----------------------------------------------------------------------------------------------------------------#
+
+##################################################################################
+# comment out the bottom code for the implementation of knn regression
+##################################################################################
+#
+    # load_data('iris.data', split, X, test)
+    # print('Length of training dataset: ' + repr(len(X)))
+    # print('Length of testing dataset: ' + repr(len(test)))
+    # predictions = []
+    # k_regress = int(input("\nEnter the value of k (knn regress): "))
+    # for x in range(len(test)):
+    #     y = myknnregress(X, test[x], k_regress)
+    #     print(y)
+    #     result = get_regress(y,k_regress)
+    #     print(result)
+    #     predictions.append(result)
+    #     print('Predicted=' + repr(result) + ', Actual=' + repr(test[x][-1]))
+    # accuracy = get_accuracy(test, predictions)
+    # print('Accuracy: ' + repr(accuracy) + '%')
+
+main()
---
+++ nb/q5.py
@@ -0,0 +1,154 @@
+# Name - Yatharth Garg
+# Class - CSE 6363
+# ID - 1001510282
+
+from math import log
+import random
+import time
+
+data = 'agaricus-lepiota.data'
+features = 'agaricus-lepiota.names'
+
+attribute_pos = []
+attribute_neg = []
+
+posdata= [] #----->edible dataset
+negdata= [] #----->poison dataset
+
+train_pos = []
+train_neg = []
+
+train = []
+test = []
+
+mushroom_features = []
+features_dictionary = {}
+
+#########################################################
+#splitting the dataset into training and testing dataset
+#########################################################
+
+
+with open(data, 'r+') as dataset_file:
+        dataset_lines = dataset_file.readlines()
+
+for line in dataset_lines:
+    attributes = line.split(',')
+
+    # Get rid of newline character on last attribute
+
+    attributes[-1] = attributes[-1].strip()
+
+    if attributes[0] == 'e':
+        posdata.append((attributes[0], attributes[1:]))
+    else:
+        negdata.append((attributes[0], attributes[1:]))
+
+temp = posdata + negdata
+train = temp[:4000]
+test = temp[4000:]
+print("The size of the training dataset:",(len(train)))
+print("The size of the testing dataset:",(len(test)))
+
+#########################################################
+#creating a separate list for attributes
+#########################################################
+
+def divide_features():
+    with open(features, 'r+') as attributes_file:
+        for line in attributes_file:
+            pair = line.strip().split()
+            mushroom_features.append(pair[0])
+            features_dictionary[pair[0]] = pair[1].split(',')
+
+def features_data():
+    attr_count = 0
+    val_count = 0
+
+    for i in range(len(mushroom_features)):
+        attribute_pos.append([])
+        attribute_neg.append([])
+
+    for i in attribute_pos:
+        for j in range(12):
+            i.append(0)
+
+    for i in attribute_neg:
+        for j in range(12):
+            i.append(0)
+
+    for attr in mushroom_features:
+        val_count = 0
+        for value in features_dictionary[attr]:
+            for example in train:
+                if value == example[1][attr_count] and example[0] == 'e':
+                    attribute_pos[attr_count][val_count] += 1
+            val_count += 1
+        attr_count += 1
+    attr_count = 0
+
+    for attr in mushroom_features:
+        val_count = 0
+        for value in features_dictionary[attr]:
+            for example in train:
+                if value == example[1][attr_count] and example[0] == 'p':
+                    attribute_neg[attr_count][val_count] += 1
+            val_count += 1
+        attr_count += 1
+
+
+
+#########################################################
+#performing the classifier
+#########################################################
+
+
+def naive_bayes(data, neg, pos):
+    count = 0
+    pos_prob = 1.0
+    neg_prob = 1.0
+
+    for i in data:
+        pos_prob *= attribute_pos[count][features_dictionary[mushroom_features[count]].index(i)]
+        neg_prob *= attribute_neg[count][features_dictionary[mushroom_features[count]].index(i)]
+        count += 1
+
+    if neg_prob > pos_prob:
+        return 'p'
+    else:
+        return 'e'
+
+if __name__ == '__main__':
+    divide_features()
+    features_data()
+
+    num_pos = 0
+    num_neg = 0
+
+    for i in range(len(train)):
+        if train[i][0] == 'e':
+            num_pos += 1
+            train_pos.append(train[i][1])
+        else:
+            num_neg += 1
+            train_neg.append(train[i][1])
+
+    correctness = 0
+
+#########################################################
+# testing the classifier on testing dataset
+#########################################################
+
+    for ex in range(len(test)):
+        actual_mushroom = test[ex][0]
+        calculated_mushroom = naive_bayes(test[ex][1], num_neg, num_pos)
+        # <------>uncomment the bottom line to manually check the accuracy of the code
+        #print('actual: %s  classified: %s' % (actual,calculated))
+        if actual_mushroom == calculated_mushroom:
+            correctness += 1
+
+#########################################################
+# displaying the results
+#########################################################
+
+    print('\nAccuracy: %f' % (float(correctness*100)/float(len(test))))
